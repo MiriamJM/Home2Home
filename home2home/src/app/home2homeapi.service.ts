aaccb3bd4ef76f56e099bd614e0c8041fedb6655
Miriam Mnyuku <mmnyuku@seattleu.edu>
2021-06-01 18:51:12 -0700

    Changed home2homeservice url

--- home2home/src/app/home2homeapi.service.ts
+++ home2home/src/app/home2homeapi.service.ts
@@ -9,44 +9,44 @@ export class Home2HomeApiService {
 
 
   getPropertiesIndex() {
-    return this.http.get('http://localhost:8080/app/properties')
-     //return this.http.get('/app/properties/')
+    //return this.http.get('http://localhost:8080/app/properties')
+     return this.http.get('/app/properties/')
     .map(response => response.json());
   }
 
   getProperties(index: string) {
-    return this.http.get('http://localhost:8080/app/properties/' + index)
-    //return this.http.get('/app/properties/' + index)
+    //return this.http.get('http://localhost:8080/app/properties/' + index)
+    return this.http.get('/app/properties/' + index)
     .map(response => response.json());
 }
 
   getUserInfoIndex() {
-    return this.http.get('http://localhost:8080/app/users')
-    //return this.http.get('/app/users/')
+    //return this.http.get('http://localhost:8080/app/users')
+    return this.http.get('/app/users/')
       .map(response => response.json());
   }
 
   getUserInfo(index: string) {
-    return this.http.get('http://localhost:8080/app/users/' + index)
-    //return this.http.get('/app/users/' + index)
+    //return this.http.get('http://localhost:8080/app/users/' + index)
+    return this.http.get('/app/users/' + index)
     .map(response => response.json());
   }
 
  getBookingInfo(index: string) {
-   //return this.http.get('http://localhost:8080/app/bookings/' + index)
+   return this.http.get('http://localhost:8080/app/bookings/' + index)
    return this.http.get('/app/bookings/' + index)
      .map(response => response.json());
    }
 
  getBookingsIndex() {
-   return this.http.get('http://localhost:8080/app/bookings/')
-   //return this.http.get('/app/bookings/')
+  // return this.http.get('http://localhost:8080/app/bookings/')
+   return this.http.get('/app/bookings/')
      .map(response => response.json());
  }
 
  getPropertySearchResults(location: string, guests: string) {
-   return this.http.get('http://localhost:8080/app/searchForHomes/' + location + "-" + guests)
-   //return this.http.get('/app/searchForHomes/' + location + "-" + guests)
+   //return this.http.get('http://localhost:8080/app/searchForHomes/' + location + "-" + guests)
+   return this.http.get('/app/searchForHomes/' + location + "-" + guests)
    .map(response => response.json());
  }
 
